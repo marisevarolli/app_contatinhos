6aeaa99d78381b175ed76ed9a3331c5ef3f0aa3f
Eloah Cavalcante Eliotério <eloahcavalcantesenai@gmail.com>
2024-09-06 11:02:55 -0300

    Feat: removemos componentes que não seriam mais utilizados e demos início a estilização da header

--- src/app/home/styles.ts
+++ src/app/home/styles.ts
@@ -1,7 +1,13 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from "react-native"
+import { theme } from "@/themes/index"
 
 export const styles = StyleSheet.create({
     container: {
-        flex:1,justifyContent: "center"
-    }
+        flex:1
+    },
+    header: {
+        width:"100%",
+        height: 132,
+        backgroundColor: theme.colors.blue,
+    },
 })
